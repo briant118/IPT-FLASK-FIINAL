d7ba01d6390712e4e7100a276b256ffa14a8244c
Bry <etoquillab@gmail.com>
2024-05-26 00:10:23 +0800

    test

---
+++ test.py
@@ -0,0 +1,30 @@
+import unittest
+from main import app
+import warnings
+
+
+class MyAppTests(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+
+    def test_index_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "<p> hello ceianyy</p>")
+
+    def test_get_branch(self):
+        response = self.app.get("/branch")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Robinson" in response.data.decode())
+
+    def test_get_branch_id(self):
+        response = self.app.get("/branch/001")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Malvar" in response.data.decode())
+
+
+if __name__ == "__main__":
+    unittest.main()
