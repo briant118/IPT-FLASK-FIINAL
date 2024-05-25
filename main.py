fe2b85952f42e02a9a7025f3797ef4de2055a79f
Bry <etoquillab@gmail.com>
2024-05-26 04:24:34 +0800

    POSt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response,  request, jsonify
 from flask_mysqldb import MySQL
 
 
@@ -34,12 +34,32 @@ def get_countries():
 def get_branch_by_id(id):
     cur = mysql.connection.cursor()
     query = """
-    SELECT * FROM branch where branchID = {} """.format(id)
+    SELECT * FROM branch where BranchID = {} """.format(id)
     cur.execute(query)
     data = cur.fetchall()
     cur.close()
 
     return make_response(jsonify(data), 200)
+
+
+@app.route("/branch", methods=["POST"])
+def add_branch():
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    Branch_location = info["Branch_Location"]
+    Branch_name = info['Branch_Name']
+    sales = int(info['Total_Sales'])
+    cur.execute("""
+        INSERT INTO branch (Branch_Name, Branch_Location, Total_Sales)
+        VALUES (%s, %s, %s)
+        """, (Branch_name, Branch_location, sales))
+    mysql.connection.commit()
+    print("row(s) affected :{}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    cur.close()
+
+    return make_response(jsonify({"message": "Branch added sucessfully",
+                                  "rows_affected": rows_affected}), 200)
 
 
 if __name__ == "__main__":
